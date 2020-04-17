e7aa206120ba0cadf9417e02a5a7066b8104c3f4
WANG Lipeng <wang.lp@outlook.com>
2020-04-18 01:06:45 +0800

    WIP jpeg decoder

--- include/jpeg_decoder.hpp
+++ include/jpeg_decoder.hpp
@@ -2,7 +2,9 @@
 #include <cstdint>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
+
 using std::string;
 using std::vector;
 
@@ -11,15 +13,69 @@ const uint8_t DC0_SYM = 0x00;
 const uint8_t DC1_SYM = 0x01;
 const uint8_t AC0_SYM = 0x10;
 const uint8_t AC1_SYM = 0x11;
+const uint8_t MARKER_PREFIX = 0xFF;
+const uint8_t SOI_SYM = 0xD8;   // start of image
+const uint8_t EOI_SYM = 0xD9;   // end of image
+const uint8_t APP0_SYM = 0xE0;  // JFIF info
+const uint8_t DQT_SYM = 0xDB;   // DQT (define quantization table)
+const uint8_t DHT_SYM = 0xC4;   // DHT (define huffman table)
+const uint8_t SOF0_SYM = 0xC0;  // start of frame (baseline)
+const uint8_t SOS_SYM = 0xDA;   // SOS, start of scan
+const uint8_t COM_SYM = 0xFE;   // comment
+
 uint16_t big_endian_bytes2_uint(void *data);
+
+struct APPinfo {
+    vector<uint8_t> identifier = vector<uint8_t>(5);
+    uint8_t version_major;
+    uint8_t version_minor;
+    uint8_t units;
+    uint16_t x_density;
+    uint16_t y_density;
+    uint8_t x_thumbnail;
+    uint8_t y_thumbnail;
+};
+
+struct ComponentInfo {
+    uint8_t horizontal_sampling;
+    uint8_t vertical_sampling;
+    uint8_t quant_table_id;
+};
+
+struct SOFInfo {
+    uint8_t precision;
+    uint16_t height;
+    uint16_t width;
+    uint8_t max_horizontal_sampling;
+    uint8_t max_vertical_sampling;
+    vector<ComponentInfo> component_infos;
+};
+
+struct HuffmanTable {
+    std::unordered_map<uint8_t, uint16_t> dc_tables[2];
+    std::unordered_map<uint8_t, uint16_t> ac_tables[2];
+};
+
+struct MCU {};
+
+struct Image_struct {
+    APPinfo app0;
+    vector<vector<float>> dqt_tables;
+    SOFInfo sof;
+};
 
 class JPEGDec {
    public:
     JPEGDec(const string &fname);
     ~JPEGDec();
     void Header(size_t idx);
+    size_t Parser_app0(size_t idx, uint8_t *data_ptr);
+    size_t Parser_DQT(size_t idx, uint8_t *data_ptr);
+    size_t Parser_SOF0(size_t idx, uint8_t *data_ptr);
+    size_t Parser_DHT(size_t idx, uint8_t *data_ptr);
 
    private:
+    vector<Image_struct> images;
     vector<vector<uint8_t>> data;
 };
 
--- src/jpeg_decoder.cpp
+++ src/jpeg_decoder.cpp
@@ -2,6 +2,15 @@
 #include <spdlog/spdlog.h>
 
 namespace jpeg_dec {
+
+void print_app0(APPinfo &app0) {
+    spdlog::info("App0:");
+    spdlog::info("\tmajor_version: {}, minor_version: {}", app0.version_major, app0.version_minor);
+    spdlog::info("\tidentifier: {} {} {} {} {}", app0.identifier[0], app0.identifier[1], app0.identifier[2],
+                 app0.identifier[3], app0.identifier[4]);
+    spdlog::info("\tunits: {}, x_density: {}, y_density: {}, x_thumbnail: {}, y_thumbnail: {}", app0.units,
+                 app0.x_density, app0.y_density, app0.x_thumbnail, app0.y_thumbnail);
+}
 uint16_t big_endian_bytes2_uint(void *data) {
     auto bytes = (uint8_t *) data;
     uint16_t res;
@@ -32,7 +41,7 @@ JPEGDec::~JPEGDec() {}
 void JPEGDec::Header(size_t idx) {
     for (size_t off = 0; off < data[idx].size(); off++) {
         uint8_t c = data[idx][off];
-        if (c != 0xFF) {
+        if (c != MARKER_PREFIX) {
             continue;
         }
         off++;  // forward one byte
@@ -43,31 +52,36 @@ void JPEGDec::Header(size_t idx) {
                 // 0xFF00 does not represent any header marker
                 break;
             };
-            case 0xD8: {
+            case SOI_SYM: {
                 spdlog::info("start of image, offset: {}", off);
                 break;
             };
-            case 0xD9: {
+            case EOI_SYM: {
                 spdlog::info("end of image, offset: {}", off);
                 return;
             };
-            case 0xE0: {
+            case APP0_SYM: {
                 spdlog::info("APP0, JFIF mark, offset: {}", off);
+                Parser_app0(idx, data[idx].data() + off + 1);
+                print_app0(images[idx].app0);
                 break;
             };
-            case 0xDB: {
+            case DQT_SYM: {
                 spdlog::info("DQT, offset: {}", off);
+                Parser_DQT(idx, data[idx].data() + off + 1);
                 break;
             };
-            case 0xC4: {
+            case DHT_SYM: {
                 spdlog::info("DHT, offset: {}", off);
+                Parser_DHT(idx, data[idx].data() + off + 1);
                 break;
             };
-            case 0xC0: {
+            case SOF0_SYM: {
                 spdlog::info("SOF0(baseline), offset: {}", off);
+                Parser_SOF0(idx, data[idx].data() + off + 1);
                 break;
             };
-            case 0xDA: {
+            case SOS_SYM: {
                 spdlog::info("SOS, begin data, offset: {}", off);
                 break;
             }
@@ -76,6 +90,128 @@ void JPEGDec::Header(size_t idx) {
             }
         }
     }
+}
+
+size_t JPEGDec::Parser_app0(size_t idx, uint8_t *data_ptr) {
+    uint8_t *data_raw = data_ptr;
+    auto len = big_endian_bytes2_uint(data_raw);
+    if (images.size() <= idx) {
+        images.resize(idx + 1);
+    }
+    data_raw += 2;
+    memcpy(images[idx].app0.identifier.data(), data_raw, 5);
+    data_raw += 5;
+    images[idx].app0.version_major = data_raw[0];
+    data_raw += 1;
+    images[idx].app0.version_minor = data_raw[0];
+    data_raw += 1;
+    images[idx].app0.units = data_raw[0];
+    data_raw += 1;
+    images[idx].app0.x_density = big_endian_bytes2_uint(data_raw);
+    data_raw += 2;
+    images[idx].app0.y_density = big_endian_bytes2_uint(data_raw);
+    data_raw += 2;
+    images[idx].app0.x_thumbnail = data_raw[0];
+    data_raw += 1;
+    images[idx].app0.y_thumbnail = data_raw[0];
+    data_raw += 1;
+    return len;
+}
+size_t JPEGDec::Parser_DQT(size_t idx, uint8_t *data_ptr) {
+    uint8_t *data_raw = data_ptr;
+    auto len = big_endian_bytes2_uint(data_ptr);
+    spdlog::info("DQT length: {}", len);
+    len -= 2;
+    data_raw += 2;
+    while (len > 0) {
+        uint8_t c = data_raw[0];
+        uint8_t id = c & 0x0F;
+        uint8_t precision = c >> 4;
+        data_raw += 1;
+        images[idx].dqt_tables.resize(id + 1);
+        images[idx].dqt_tables[id].resize(64);
+        float *table = images[idx].dqt_tables[id].data();
+        if (precision == 0) {
+            for (int i = 0; i < 64; i++) {
+                table[i] = data_raw[i];
+            }
+            data_raw += 64;
+            len -= 65;
+        } else if (precision == 1) {
+            for (int i = 0; i < 64; i++) {
+                table[i] = big_endian_bytes2_uint(data_raw + i * 2);
+            }
+            data_raw += 128;
+            len -= 129;
+        } else {
+            spdlog::error("Unknown DQT table");
+            exit(1);
+        }
+        printf("DQT table %d, precision %d\n", id, precision);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                printf("%.2f ", table[i * 8 + j]);
+            }
+            printf("\n");
+        }
+    }
+    return len;
+}
+
+size_t JPEGDec::Parser_SOF0(size_t idx, uint8_t *data_ptr) {
+    uint8_t *data_raw = data_ptr;
+    auto len = big_endian_bytes2_uint(data_raw);
+    data_raw += 2;
+    spdlog::info("SOF0 length: {}", len);
+    images[idx].sof.precision = data_raw[0];
+    data_raw += 1;
+    images[idx].sof.height = big_endian_bytes2_uint(data_raw);
+    data_raw += 2;
+    images[idx].sof.width = big_endian_bytes2_uint(data_raw);
+    data_raw += 2;
+
+    uint8_t num_component = data_raw[0];
+    data_raw += 1;
+
+    printf("precision: %d, height: %d, width: %d\n", images[idx].sof.precision, images[idx].sof.height,
+           images[idx].sof.width);
+
+    images[idx].sof.max_horizontal_sampling = 0;
+    images[idx].sof.max_vertical_sampling = 0;
+    for (uint8_t i = 0; i < num_component; i++) {
+        uint8_t component_id = data_raw[i];
+        data_raw += 1;
+        ComponentInfo cinfo;
+        uint8_t c = data_raw[0];
+        data_raw += 1;
+        cinfo.horizontal_sampling = c >> 4;
+        cinfo.vertical_sampling = c & 0x0F;
+        cinfo.quant_table_id = data_raw[0];
+        data_raw += 1;
+        if (cinfo.horizontal_sampling > images[idx].sof.max_horizontal_sampling) {
+            images[idx].sof.max_horizontal_sampling = cinfo.horizontal_sampling;
+        }
+        if (cinfo.vertical_sampling > images[idx].sof.max_vertical_sampling) {
+            images[idx].sof.max_vertical_sampling = cinfo.vertical_sampling;
+        }
+        images[idx].sof.component_infos.push_back(cinfo);
+        printf("horizontal_sampling: %d, vertical_sampling: %d, quant_table_id: %d\n", cinfo.horizontal_sampling,
+               cinfo.vertical_sampling, cinfo.quant_table_id);
+        // printf("");
+    }
+
+    spdlog::info("max horizontal sampling: {}, max vertical sampling: {}", images[idx].sof.max_horizontal_sampling,
+                 images[idx].sof.max_vertical_sampling);
+
+    return len;
+}
+
+size_t JPEGDec::Parser_DHT(size_t idx, uint8_t *data_ptr) {
+    uint8_t *data_raw = data_ptr;
+    auto len = big_endian_bytes2_uint(data_raw);
+    data_raw += 2;
+    spdlog::info("DHT length: {}", len);
+    return len;
 }
 
 }  // namespace jpeg_dec
