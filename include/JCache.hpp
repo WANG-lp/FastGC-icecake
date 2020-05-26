9c1a1770032ee705fcd7057a2fbdd6fcb1a0069e
WANG Lipeng <wang.lp@outlook.com>
2020-05-26 18:13:42 +0800

    WIP: thrift rpc server

--- include/JCache.hpp
+++ include/JCache.hpp
@@ -5,8 +5,20 @@
 
 #include "jpeg_decoder_def.hpp"
 
+#include <thrift/concurrency/ThreadFactory.h>
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/server/TThreadPoolServer.h>
+#include <thrift/transport/TSocket.h>
+
+#include "../src/gen-cpp/JPEGCache.h"
+
 using std::string;
 using std::vector;
+
+namespace at = ::apache::thrift;
+namespace atp = ::apache::thrift::protocol;
+namespace att = ::apache::thrift::transport;
+using namespace ::JPEGCache;
 
 namespace jcache {
 class JCache {
@@ -18,7 +30,21 @@ class JCache {
     bool putJPEG(const string &filename);
     JPEG_HEADER *getHeader(const string &filename);
     JPEG_HEADER *getHeaderwithCrop(const string &filename, int offset_x, int offset_y, int roi_width, int roi_height);
+
    private:
     std::unordered_map<string, JPEG_HEADER> map_;
 };
+
+class JPEGCacheHandler : virtual public JPEGCacheIf {
+   public:
+    JPEGCacheHandler(){};
+    ~JPEGCacheHandler() = default;
+    void get(std::string &_return, const std::string &filename){
+
+    };
+    void getWithROI(std::string &_return, const std::string &filename, const int32_t offset_x, const int32_t offset_y,
+                    const int32_t roi_w, const int32_t roi_h) {}
+    int32_t put(const std::string &filenames) { return 0; }
+};
+
 }  // namespace jcache
---
+++ src/thrift-idls/JPEGCache.thrift
@@ -0,0 +1,8 @@
+namespace cpp JPEGCache
+
+
+service JPEGCache {
+    binary get(1:string filename)
+    binary getWithROI(1:string filename, 2:i32 offset_x, 3:i32 offset_y, 4:i32 roi_w, 5:i32 roi_h)
+    i32 put(1:string filenames)
+}
